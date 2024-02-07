79daf01c9256e2ebe7fbd71b8b79512cd2cc8ab3
Ysnaldo Lopez <ysnaldo.lopez@masiv.com>
2024-02-06 21:05:29 -0500

    first commit

---
+++ LambdaPipelines/LambdaPipelines/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+namespace LambdaPipelines;
+
+public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+{
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder
+            .UseStartup<Startup>();
+    }
+
+    protected override void Init(IHostBuilder builder)
+    {
+    }
+}
