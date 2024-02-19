44256bc8c43017ff08a43d2982c70c72c60c0238
Ysnaldo Lopez <ysnaldo.lopez@masiv.com>
2024-02-19 10:50:48 -0500

    Added CDK configs

---
+++ LambdaPipelines/CDK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Amazon.CDK;
+using CDK.Stacks;
+
+namespace CDK;
+
+[ExcludeFromCodeCoverage]
+public static class Program
+{
+    public static void Main()
+    {
+        IEnvironment environment = new Amazon.CDK.Environment
+        {
+            Account = "",
+            Region = "us-east-1"
+        };
+        var app = new App();
+        _ = new LambdaPipelinesStack(app, "LambdaPipelinesStack", new StackProps { Env = environment });
+
+        app.Synth();
+    }
+}
